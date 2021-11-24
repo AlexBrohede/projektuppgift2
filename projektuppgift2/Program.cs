aefcc39c459d9f4e34380adafed0deefe79d83bf
Alexander Brohede <04ALBRF@skola.lidkoping.se>
2021-11-24 14:23:13 +0100

    start on main menu

--- projektuppgift2/Program.cs
+++ projektuppgift2/Program.cs
@@ -7,6 +7,11 @@ namespace projektuppgift2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int menu1 = int.Parse(Console.ReadLine());
+            while (menu1 != 5)
+            {
+
+            }
         }
     }
 }

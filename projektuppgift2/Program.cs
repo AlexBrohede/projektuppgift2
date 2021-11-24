0a8ae15214e7dbedc19fba6a59f30cc0ea55fc8a
Alexander Brohede <04albrf@skola.lidkoping.se>
2021-11-24 13:50:44 +0100

    Add project files.

---
+++ projektuppgift2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projektuppgift2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

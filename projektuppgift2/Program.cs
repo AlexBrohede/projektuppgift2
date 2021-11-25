b7f4ed61efb28745786d67a18fda89902409168b
Alexander Brohede <04ALBRF@skola.lidkoping.se>
2021-11-25 13:00:13 +0100

    Added more to the main menu Added a shut down option

--- projektuppgift2/Program.cs
+++ projektuppgift2/Program.cs
@@ -1,16 +1,40 @@
 ﻿using System;
-
+using System.Threading;
 namespace projektuppgift2
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            int menu1 = int.Parse(Console.ReadLine());
-            while (menu1 != 5)
+        {      
+            int menu1 = 0;
+            while (menu1 != 4)
             {
+                Console.WriteLine($"menu1 \n4: stop");
+                menu1 = int.Parse(Console.ReadLine());
+                if (menu1 == 1)
+                {
 
+                }
+                else if (menu1 == 2)
+                {
+
+                }
+                else if (menu1 == 3)
+                {
+
+                }
+                else if (menu1 == 4)
+                {
+                    Console.WriteLine($"Stopping the simulation");
+                    Thread.Sleep(1500);
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("That was not an option \nPress any key to pick again");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
             }
         }
     }

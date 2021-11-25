e6afd991f1680686082eef774bd13292d47e4fd4
Alexander Brohede <04ALBRF@skola.lidkoping.se>
2021-11-25 13:48:06 +0100

    Added text management Added reset button Patched File reset

--- projektuppgift2/Program.cs
+++ projektuppgift2/Program.cs
@@ -7,14 +7,62 @@ namespace projektuppgift2
     {
         static void Main(string[] args)
         {
+            string[] Test = new string[6];
             int menu1 = 0;
-            while (menu1 != 4)
-            {
-                Console.WriteLine($"menu1 \n4: stop");
-                menu1 = int.Parse(Console.ReadLine());
-                if (menu1 == 1)
+            while (menu1 != 5)
+            {              
+                if (File.Exists("TestFile.txt"))
                 {
-                    string[] Test = new string[6];
+                    Console.WriteLine($"1: Test File sys \n2: Manage text \n3: ??? \n4: Reset \n5: stop");
+                    menu1 = int.Parse(Console.ReadLine());
+                    if (menu1 == 1)
+                    {
+                        string[] Filecontent = File.ReadAllLines("TestFile.txt");
+                        foreach (string Line in Filecontent)
+                        {
+                            Console.WriteLine(Line);
+                        }
+                        Console.WriteLine();
+                    }
+                    else if (menu1 == 2)
+                    {
+                        Console.WriteLine($"Choose array position \n1-6");
+                        int ArrayPosition = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Change text in position {ArrayPosition}");
+                        Test[ArrayPosition - 1] = Console.ReadLine();
+                        File.WriteAllLines("TestFile.txt", Test);
+                    }
+                    else if (menu1 == 3)
+                    {
+
+                    }
+                    else if (menu1 == 4)
+                    {
+                        {
+                            Test[0] = "Hi1";
+                            Test[1] = "Hi2";
+                            Test[2] = "Hi3";
+                            Test[3] = "Hi4";
+                            Test[4] = "Hi5";
+                            Test[5] = "Hi6";
+                        }
+                        File.WriteAllLines("TestFile.txt", Test);
+                    }
+                    else if (menu1 == 5)
+                    {
+                        Console.WriteLine($"Stopping the simulation");
+                        Thread.Sleep(1500);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine("That was not an option \nPress any key to pick again");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+                else
+                {
                     {
                         Test[0] = "Hi1";
                         Test[1] = "Hi2";
@@ -24,32 +72,6 @@ namespace projektuppgift2
                         Test[5] = "Hi6";
                     }
                     File.WriteAllLines("TestFile.txt", Test);
-                    string[] Filecontent = File.ReadAllLines("TestFile.txt");
-                    foreach (string Line in Filecontent)
-                    {
-                        Console.WriteLine(Line);
-                    }
-                    Console.WriteLine();
-                }
-                else if (menu1 == 2)
-                {
-
-                }
-                else if (menu1 == 3)
-                {
-
-                }
-                else if (menu1 == 4)
-                {
-                    Console.WriteLine($"Stopping the simulation");
-                    Thread.Sleep(1500);
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine("That was not an option \nPress any key to pick again");
-                    Console.ReadKey();
-                    Console.Clear();
                 }
             }
         }
